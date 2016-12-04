03cce0b7c6ef2a95668dd5619ca9fe83a54b3a8a
krashaen <kmgqq@mail.ru>
2016-12-04 11:31:11 +0700

    refactoring header menu

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -3,4 +3,9 @@ function showMenu() {
   menu[0].className === 'menu' ?
     menu[0].className = 'menu menu_visible'
     : menu[0].className = 'menu';
+
+  var menuButton = document.getElementsByClassName('nav-bar__menu-button');
+  menuButton[0].className === 'nav-bar__menu-button' ?
+    menuButton[0].className = 'nav-bar__menu-button nav-bar__menu-button_active'
+    : menuButton[0].className = 'nav-bar__menu-button';
 }
